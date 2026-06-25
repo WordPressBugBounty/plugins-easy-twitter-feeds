dea9476f974bfab075a0589bc6cb2bfa9932d3de
Sean Murphy <sean@iamseanmurphy.com>
2026-06-25 11:11:01 +0000

    1.2.14

--- easy-twitter-feeds/build/admin-dashboard.asset.php
+++ easy-twitter-feeds/build/admin-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data'), 'version' => 'a3a19703c9c80a52b841');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '815596a07abf05883d0a');
